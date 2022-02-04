dfd76f25adedd3928cc7d2bf4beb030679478d0c
numan96 <numan1@hotmail.co.uk>
2022-02-04 00:32:39 +0000

    Work on welcome and training pages.
    - Add tabs to show new/past exercises.
    - Card design to new exercise to select a workout and start a spinner timer.
    - Dialog box on stop button for current exercise which receives percentage value data from component. 'Cancel' will carry on timer, 'Yes' will go back to new exercise card.
    - Add Logo to header.

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { StopTrainingComponent } from './stop-training.component';
 
 @Component({
   selector: 'app-current-training',
   templateUrl: './current-training.component.html',
-  styleUrls: ['./current-training.component.scss']
+  styleUrls: ['./current-training.component.scss'],
 })
 export class CurrentTrainingComponent implements OnInit {
+  @Output() trainingExit = new EventEmitter();
+  progress = 0;
+  timer: number = 0;
 
-  constructor() { }
+  constructor(private _dialog: MatDialog) {}
 
   ngOnInit(): void {
+    this.startOrResumeTimer();
   }
 
+  startOrResumeTimer() {
+    this.timer = setInterval(() => {
+      this.progress = this.progress + 5;
+      if (this.progress >= 100) {
+        clearInterval(this.timer);
+      }
+    }, 1000);
+  }
+  onStop() {
+    clearInterval(this.timer);
+
+    const dialogRef = this._dialog.open(StopTrainingComponent, {
+      data: {
+        progress: this.progress,
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.trainingExit.emit();
+      } else {
+        this.startOrResumeTimer();
+      }
+    });
+  }
 }
